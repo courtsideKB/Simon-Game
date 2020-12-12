f28f5bd101bfc0350074d91c5cd4cfe048409bbe
courtsideKB <kbankhead1992.yahoo.com>
2020-12-11 23:24:42 -0800

    Wrote the animation code and the functionality of having a sound played when a button is selected

--- game.js
+++ game.js
@@ -1,1 +1,20 @@
-alert(".....");
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+/* Generate random number for sequence */
+
+function nextSequence() {
+    var randomNumber = Math.random()
+    randomNumber = Math.floor(randomNumber * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+ 
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var media = new Audio('Sounds/' + randomChosenColor + ".mp3");
+    media.play();
+}
+
+
+
